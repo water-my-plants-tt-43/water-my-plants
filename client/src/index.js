58a703108ef050e96d93d1649aabb05bc2164bea
Eduardo Lopez <ed98lopez@gmail.com>
2021-03-31 10:17:21 -0700

    wrapped App component in ThemeContext.Provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
+import {ThemeContext, theme} from './context/ThemeContext'
 
 
 ReactDOM.render(
   <Router>
+    <ThemeContext.Provider value={theme}>
     <App />
+    </ThemeContext.Provider>
   </Router>,
   document.getElementById('root')
 );
