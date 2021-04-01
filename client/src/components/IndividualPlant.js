8ff0fb0cbb67754e3423003c59b83c5e32a574e3
Eduardo Lopez <ed98lopez@gmail.com>
2021-04-01 16:25:36 -0700

    individual plants is styled

--- client/src/components/IndividualPlant.js
+++ client/src/components/IndividualPlant.js
@@ -1,6 +1,74 @@
 import React, { useState, useEffect } from "react";
+import Button from './Button'
+import styled from 'styled-components'
+import thumb from '../plantThumbImg.svg'
 import { useParams, useHistory } from "react-router-dom";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const Wrapper= styled.div`
+width:100%;
+height:80vh;
+overflow:hidden;
+display: flex;
+justify-content:center;
+align-items:center;
+font-family:${props=>props.theme.font.body};
+font-weight:400;
+color: ${props=>props.theme.color.grayDark};
+
+& .infoContainer{
+  width:80%;
+  margin:auto;
+  display:flex;
+  justify-content: center;
+  align-items:center;
+}
+
+& .info{
+  margin:2rem;
+}
+
+& .imgContainer{
+  width: 300px;
+  margin:2rem;
+  
+
+  & img{
+    width:100%;
+    height:auto;
+    object-fit:cover;
+  }
+};
+
+& button{
+  font-size: 1.2rem;
+}
+
+& .large{
+  
+  width:70%;
+  position:absolute;
+  z-index: 1;
+  color: ${props=>props.theme.color.teal};
+  left:0;
+  margin:0;
+  padding:0;
+  opacity:.12;
+  top:35%;
+  transform-origin: center center;
+  transform: translate(0,-50%);
+
+  & h1{
+  font-family:${props=>props.theme.font.display};
+  font-weight:400;
+  width:100%;
+  font-size:15rem;
+  margin:0;
+  padding:0;
+  }
+
+}
+`
 
 const IndividualPlant = (props) => {
   // const {
@@ -24,20 +92,27 @@ const IndividualPlant = (props) => {
   }, []);
 
   return (
-    <div className='plant-list-item'>
-      <div className='plant-view'>
-        <div className='view-header'>
-          <h1>{plant.nickname}</h1>
+    <Wrapper>
+
+      <div className='large'><h1>{plant.nickname}</h1></div>
+      <div className= 'infoContainer'>
+      <div className='imgContainer'>
+        <img src={thumb} alt='plant'/>
+      </div>
+
+
+      
+        <div className='info'>
+          <h3>{plant.nickname}</h3>
           <p>{plant.last_water && plant.last_water.slice(0,10)}</p>
           <p>{plant.species}</p>
           <p>Created By: {plant.user}</p>
           <p>Days between watering: {plant.water_frequency}</p>
-          <button onClick={() => push(`/plant/${plant.user_plants_id}/edit`)}>
-            Edit {plant.nickname}
-          </button>
+          <Button onClick={() => push(`/plant/${plant.user_plants_id}/edit`)} innerText={`Edit ${plant.nickname}`}/>
         </div>
-      </div>
-    </div>
+        </div>
+      
+    </Wrapper>
   );
 };
 
