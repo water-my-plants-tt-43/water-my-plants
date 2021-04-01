651e14dacec26c0fe679c13d2388621aa295cda4
Trenten Grede <trenten.grede@gmail.com>
2021-03-31 23:01:34 -0500

    fixed up the backend endpoints and added restricted middleware on everything aside from the login and register

--- backend/api/plants/plants-router.js
+++ backend/api/plants/plants-router.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const Plants = require('./plants-model')
+const {restricted} = require('../auth/auth-middleware')
 
-router.get('/', async (req, res) => {
+router.get('/', restricted, async (req, res) => {
   const plants = await Plants.find()
   res.status(200).json(plants)
 })
