5c2e88968e113809c6b51996fb6bb7f7ab3e59c0
Nicholas Smith <nicholassmithbus@gmail.com>
2021-04-01 09:28:26 -0600

    general changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-
 import Home from "./components/Home";
 import RegisterForm from "./components/RegisterForm";
 import LoginForm from "./components/LoginForm";
 import PlantsList from "./components/PlantsList";
 import NewPlant from "./components/NewPlant";
+import IndividualPlant from "./components/IndividualPlant";
+// import EditPlant from "./components/EditPlant";
 
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
@@ -14,22 +15,24 @@ function App() {
 
   return (
     <div>
-      
-      
       <Switch>
         <Route path={"/RegisterForm"}>
           <RegisterForm />
         </Route>
-        <Route path='/login' >
+        <Route path='/login'>
           <LoginForm setUserId={setUserId} />
         </Route>
-        <PrivateRoute path='/plants' component={()=><PlantsList userId={userId} setUserId={setUserId}/>} />
+        <PrivateRoute
+          path='/plants'
+          component={() => <PlantsList userId={userId} setUserId={setUserId} />}
+        />
         <PrivateRoute path='/newplant' component={NewPlant} />
+        <PrivateRoute path='/plant/:plantId' component={IndividualPlant} />
+        {/* <PrivateRoute path='/plant/:plantId/edit' component={EditPlant} /> */}
         <Route path='/'>
           <Home />
         </Route>
       </Switch>
-      
     </div>
   );
 }
